6fad6a05e1430b78f13f226309f0e81cb89416b0
LisaCee <lisaceedev@gmail.com>
2026-05-31 15:49:00 -0700

    Add footer

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import TopNav from './TopNav'
+import Footer from "../sections/Footer";
 
 export default function Layout() {
 	return (
@@ -8,6 +9,7 @@ export default function Layout() {
 			<main className="pt-20 p-8 max-w-lg md:max-w-4xl mx-auto flex flex-col gap-8">
 				<Outlet />
 			</main>
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,19 @@
+import { FaInstagram } from "react-icons/fa";
+
+export default function Footer() {
+	return (
+		<footer className="bg-background flex justify-between max-w-7xl py-3 px-5 mx-auto mt-12">
+			<p className="text-foreground text-xs font-[GeneralSans-Light]">&copy; {new Date().getFullYear()} Jakobi Art Works</p>
+			<a
+				href="https://www.instagram.com/j.a.w/"
+				target="_blank"
+				rel="noopener noreferrer"
+				aria-label="Instagram"
+				className="text-foreground transition-colors"
+			>
+				<FaInstagram size={18} />
+				<span className="sr-only">Instagram - opens in new link</span>
+			</a>
+		</footer>
+	);
+}
