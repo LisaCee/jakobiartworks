e19a562f92017019541d4a9b15971cf81dedf808
LisaCee <lisaceedev@gmail.com>
2026-05-31 16:05:44 -0700

    Gallery hover effect

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -9,16 +9,17 @@ export default function Home() {
 
 			{/* Masonry gallery */}
 			<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-6 grid-flow-dense">
-				{artworks.map((artwork) => (
+				{artworks.map((artwork, index) => (
 					<div
 						key={artwork.id}
-						className={`aspect-square ${artwork?.featured ? "col-span-2 row-span-2" : ""} p-4`}
-						onClick={() => console.log("clicked", artwork.id)}
+						className={`aspect-square overflow-hidden ${artwork?.featured ? "col-span-2 row-span-2" : ""} p-4`}
+						style={{ animationDelay: `${index * 40}ms` }}
 					>
 						<img
 							src={artwork.src}
 							alt={artwork.alt}
-							className="w-full h-auto object-cover"
+							className="w-full h-auto object-cover transition-all duration-300 hover:shadow-[3px_3px_8px_rgba(0,0,0,0.3)]
+ hover:scale-[1.05]"
 						/>
 					</div>
 				))}
